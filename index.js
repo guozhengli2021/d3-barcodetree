b8af7ee9c84e8f3698520b7f2cae778d3670fad0
GeorgeProjects <liguozhengsdu@gmail.com>
2018-12-02 15:55:06 +0800

    initialize the website of BarcodeTree

--- index.js
+++
@@ -1,1 +0,0 @@
-export {default as barcodetree} from "./src/barcodetree";
--- test/barcodetree-test.js
+++
@@ -1,7 +0,0 @@
-var tape = require("tape"),
-    barcodetree = require("../");
-
-tape("barcodetree() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-  // test.equal(barcodetree.barcodetree(), 42);
-  test.end();
-});
